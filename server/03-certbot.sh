7c245a237110b9c1497894a33f0c91155e5e76b4
nycpivot <developer@thejameshome.net>
2024-04-30 13:52:42 -0400

    Updated certbot script.

--- server/03-certbot.sh
+++ server/03-certbot.sh
@@ -8,15 +8,15 @@ sudo apt-get install python3-certbot-nginx
 # need to run this in order to install all the files
 # in the /etc/letsencypt folder because these files are
 # referred to in the nginx.conf file
-sudo certbot --nginx -d media.heirwaves.net
+sudo certbot --nginx -d media.frontlets.net
 
 # custom - replace conf file with media server settings
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.backup
-sudo cp /home/ubuntu/heirwaves-operations/config/nginx.conf /etc/nginx
+sudo cp /home/ubuntu/frontlets-operations/config/nginx.conf /etc/nginx
 # custom
 
 sudo service nginx restart
 
 # run this again now all the necessary files are in place?!?
 # it's lame but it works
-sudo certbot --nginx -d media.heirwaves.net
+sudo certbot --nginx -d media.frontlets.net
